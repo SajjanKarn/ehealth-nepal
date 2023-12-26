9db87fc3f4cf960805d32a607031ef446ae1e57c
Yaman Sarabariya <yaman.sarabariya@gmail.com>
2023-12-26 10:41:58 +0545

    design homepage

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -1,4 +1,8 @@
 import { Navigation } from "@/components/Navigation.component";
+import styles from "./page.module.css"
+import { Button } from "@/components/Button.component";
+import Image from "next/image";
+import HeroImage from "@/public/hero.svg"
 
 export default function Home() {
   return (
@@ -6,6 +10,25 @@ export default function Home() {
       
       {/* Navigation */}
       <Navigation />
+
+      <div className={styles.container}>
+
+        <h1>Welcome To EHealth.</h1>
+
+        <div className={styles.action}>
+              <Button variant="primary" >
+                Register
+              </Button>
+              <Button variant="secondary">
+                Login
+              </Button>
+        </div>
+
+        <div>
+          <Image src={HeroImage} height={400} />
+        </div>
+
+      </div>
 
     </main>
   );
